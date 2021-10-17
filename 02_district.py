1e8e86c9981a5422c6939857dbe0818bd470ee4b
Bondarenko <Homja4iwe@gmail.com>
2021-10-17 17:43:31 +0300

    Выполнил задание №2

--- 02_district.py
+++ 02_district.py
@@ -5,7 +5,23 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1 import room1 as inhabitants_1
+from district.central_street.house1 import room2 as inhabitants_2
+from district.central_street.house2 import room1 as inhabitants_3
+from district.central_street.house2 import room2 as inhabitants_4
+from district.soviet_street.house1 import  room1 as inhabitants_5
+from district.soviet_street.house1 import  room1 as inhabitants_6
+from district.soviet_street.house1 import  room2 as inhabitants_7
+from district.soviet_street.house1 import  room2 as inhabitants_8
+
+
+print('На районе живут: ', ' , '.join(inhabitants_1.folks), ' , '.join(inhabitants_2.folks), ' , '
+                                                                                             ''.join(
+    inhabitants_3.folks), ' , '.join(inhabitants_4.folks), ' , '.join(inhabitants_5.folks), ' , '
+                                                                                            ''.join(
+    inhabitants_6.folks), ' , '.join(inhabitants_7.folks), ' , '.join(inhabitants_8.folks))
+
+
 
 
 
