c813706d691871484dc4b2b73ffcf238938a1a5e
Bondarenko <Homja4iwe@gmail.com>
2021-10-17 16:13:14 +0300

    Выполнил задание №1

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -3,9 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-from room_1 import folks
+from room_1 import folks as room1
+from room_2 import folks as room2
 
-print('В комнате room_1 живут: ', ' , '.join(folks))
+print('В комнате room_1 живут: ', ' , '.join(room1))
 
-from room_2 import folks
-print('В комнате room_2 живут: ', ' , '.join(folks))
+print('В комнате room_2 живут: ', ' , '.join(room2))
